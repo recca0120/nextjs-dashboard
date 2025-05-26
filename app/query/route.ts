3d7f2ef49a52a19dd911f2c9a188b053f257d093
recca0120 <recca0120@gmail.com>
2025-05-26 10:12:03 +0800

    chapter 15

--- app/query/route.ts
+++ app/query/route.ts
@@ -13,9 +13,15 @@ async function listInvoices() {
     return data;
 }
 
+async function listUsers() {
+    const data = await sql`SELECT * FROM users`;
+
+    return data;
+}
+
 export async function GET() {
     try {
-        return Response.json(await listInvoices());
+        return Response.json(await listUsers());
     } catch (error) {
         return Response.json({ error }, { status: 500 });
     }
